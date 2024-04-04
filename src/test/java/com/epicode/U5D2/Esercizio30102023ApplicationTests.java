f22bf277b049c36b9606e91c57e590daea99d6ce
Davide Paderi 98 <151513202+DavidePad98@users.noreply.github.com>
2024-04-04 16:23:59 +0200

    start

---
+++ src/test/java/com/epicode/U5D2/Esercizio30102023ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.epicode.U5D2;
+
+import com.epicode.U5D2.entities.Menu;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+class Esercizio30102023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
